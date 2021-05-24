01d2bf3cf5b8143fb3229d243e19a6ad8256e338
Nick <drahnakn@comcast.net>
2021-05-23 23:01:00 -0600

    Updated files for week 3 assignment

--- week3/src/app.ts
+++ week3/src/app.ts
@@ -1,1 +1,40 @@
-// Source code here
+export interface Marsupial {
+    habitat: String,
+    dietType: String,
+    avgWeight: number,
+    avgLength: number
+
+    getHabitat: () => String;
+    isOmnivorous: (dietType: String) => boolean;
+}
+
+export class Koala implements Marsupial {
+    species: String;
+    
+    constructor(public habitat: String, public dietType: String, public avgWeight: number, public avgLength: number) {
+        this.species = "Koala";
+        this.habitat = habitat;
+        this.dietType = dietType;
+        this.avgWeight = avgWeight;
+        this.avgLength = avgLength
+    }
+
+    getHabitat(): String {
+        return `${this.species}s can be found in the ${this.habitat}.`;
+    }
+
+    isOmnivorous(dietType: String): boolean {
+        if(dietType === "Omnivore") {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
+
+const koala = new Koala("Eucalyptus forests of Southeastern and Eastern Australia", "Herbivore", 35, 32);
+
+console.log(koala.getHabitat());
+console.log(koala.isOmnivorous(koala.dietType));
+
+
--- week3/src/observable.ts
+++ week3/src/observable.ts
@@ -1,1 +1,50 @@
-// Source code here
+import { Observable } from "rxjs/internal/Observable";
+import { Observer } from "rxjs/internal/types"; // eslint-disable-line no-unused-vars
+
+const marsupialSpecies = 
+    [{name: "Bandicoot", status: "least concern"},
+     {name: "Koala", status: "threatened"},
+     {name: "Numbat", status: "vulnereable"},
+     {name: "Quokka", status: "vulnerable"},
+     {name: "Quoll", status: "endangered"},
+     {name: "Red Kangaroo", status: "least concern"},
+     {name: "Wombat", status: "endangered"}];
+
+export class Marsupials {
+
+    // constructor() {}
+
+    getMarsupialSpecies(observer: Observer<Object>) {
+
+        for (let species of marsupialSpecies) {
+            observer.next(species); 
+        }
+
+    }
+
+    getEndangeredMarsupials(marsupials: Array<any>) {
+        
+        return marsupials.filter(species => species.status === "endangered").map(species => species.name);
+    }
+
+    isEndangered(marsupials: Array<any>, name: string) {
+        
+        if (marsupials.find(species => species.name === name).status === "endangered") {
+            return true;
+        } else return false;
+    }
+
+}
+
+const marsupials = new Marsupials();
+let marsupialNames: Array<any> = [];
+
+const marsupialSpeciesList = new Observable(marsupials.getMarsupialSpecies);
+
+marsupialSpeciesList.subscribe((data) =>
+    {marsupialNames.push(data);}).unsubscribe();
+
+console.log(marsupialNames);
+const endangeredMarsupials = marsupials.getEndangeredMarsupials(marsupialNames);
+console.log(endangeredMarsupials);
+console.log(marsupials.isEndangered(marsupialNames, "Koala"));
