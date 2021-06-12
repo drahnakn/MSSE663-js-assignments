7a70c6f879cf19a6760e07ab9191f701faf6fa8b
Nicholas Drahnak <Nicholas_Drahnak@cable.comcast.com>
2021-06-12 09:58:28 -0600

    week 3 assignment.

--- week3/src/app.spec.ts
+++ week3/src/app.spec.ts
@@ -1,18 +1,25 @@
-import { expect } from 'chai';
-import 'mocha';
-import { Koala } from './app';
+import { expect } from "chai";
+import "mocha";
+import { Koala } from "./app";
 
-describe('test for app.ts' , () => {
-    const koala = new Koala('Eucalyptus forests of Southeastern and Eastern Australia', 'Herbovore', 35, 32);
-    it('Should display the habitat for a koala', () => {
-        expect(koala.getHabitat()).to.equal('Koalas can be found in the Eucalyptus forests of Southeastern and Eastern Australia.');
-    });
+describe("test for app.ts", () => {
+  const koala = new Koala(
+    "Eucalyptus forests of Southeastern and Eastern Australia",
+    "Herbovore",
+    35,
+    32
+  );
+  it("Should display the habitat for a koala", () => {
+    expect(koala.getHabitat()).to.equal(
+      "Koalas can be found in the Eucalyptus forests of Southeastern and Eastern Australia."
+    );
+  });
 
-    it('Should indicate if a koala is an omnivore', () => {
-        expect(koala.isOmnivorous(koala.dietType)).to.equal(false);
-    });
+  it("Should indicate if a koala is an omnivore", () => {
+    expect(koala.isOmnivorous(koala.dietType)).to.equal(false);
+  });
 
-    it('Should display a koala\'s average weight', () => {
-        expect(koala.avgWeight).to.equal(35);
-    });
+  it("Should display a koala's average weight", () => {
+    expect(koala.avgWeight).to.equal(35);
+  });
 });
--- week3/src/observable.spec.ts
+++ week3/src/observable.spec.ts
@@ -1,32 +1,33 @@
-import { expect } from 'chai';
-import 'mocha';
-import { Marsupials } from './observable';
+import { expect } from "chai";
+import "mocha";
+import { Marsupials } from "./observable";
 
-
-describe('test for observable.ts', () => {
-    const marsupials = new Marsupials();
-    const marsupialList = [
-        {
-            name: "Koala",
-            status: "threatened"
-        },
-        {
-            name: "Wombat", 
-            status: "endangered"
-        },
-        {
-            name: "Quoll", 
-            status: "endangered"
-        },
-    ]
-    // const expectedEndangeredAnmials = ["Wombat"];
-    it('should indicate false when determining is a koala is enadangered', () => {
-        expect(marsupials.isEndangered(marsupialList, "Koala")).to.equal(false);
-    })
-    it('should indicate true when determining is a wombat is enadangered', () => {
-        expect(marsupials.isEndangered(marsupialList, "Wombat")).to.equal(true);
-    })
-    it('should return an array of endangered marsupials', () => {
-        expect(marsupials.getEndangeredMarsupials(marsupialList)).to.contain('Wombat');
-    })
-})
+describe("test for observable.ts", () => {
+  const marsupials = new Marsupials();
+  const marsupialList = [
+    {
+      name: "Koala",
+      status: "threatened",
+    },
+    {
+      name: "Wombat",
+      status: "endangered",
+    },
+    {
+      name: "Quoll",
+      status: "endangered",
+    },
+  ];
+  // const expectedEndangeredAnmials = ["Wombat"];
+  it("should indicate false when determining is a koala is enadangered", () => {
+    expect(marsupials.isEndangered(marsupialList, "Koala")).to.equal(false);
+  });
+  it("should indicate true when determining is a wombat is enadangered", () => {
+    expect(marsupials.isEndangered(marsupialList, "Wombat")).to.equal(true);
+  });
+  it("should return an array of endangered marsupials", () => {
+    expect(marsupials.getEndangeredMarsupials(marsupialList)).to.contain(
+      "Wombat"
+    );
+  });
+});
